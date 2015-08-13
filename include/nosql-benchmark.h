633ad2bf605fcd24eca6f005f910e43f081ff7c3
Dmitriy Kalugin-Balashov <rvncerr@rvncerr.org>
2015-08-13 16:39:11 +0000

    New version.

---
+++ include/nosql-benchmark.h
@@ -0,0 +1,129 @@
+#ifndef NOSQL_BENCHMARK_H
+#define NOSQL_BENCHMARK_H
+
+#include <stdlib.h>     // malloc, free.
+#include <string.h>     // memset.
+#include <sys/types.h>  // getaddrinfo, socket.
+#include <sys/socket.h> // getaddrinfo, socket.
+#include <netdb.h>      // getaddrinfo.
+#include <unistd.h>     // close.
+#include <stdio.h>      // sprintf.
+#include <pthread.h>    // pthread_create.
+#include <sys/epoll.h>  // epoll_create, epoll_ctl, epoll_wait.
+#include <stdarg.h>     // va_list.
+
+#include <msgpuck/msgpuck.h>
+
+#define NOSQL_TYPE_TARANTOOL 0
+#define NOSQL_TYPE_REDIS     1
+#define NOSQL_TYPE_MEMCACHED 2
+#define NOSQL_TYPE_DYNAMODB  3
+#define NOSQL_TYPE_AEROSPIKE 4
+
+typedef struct nosql_s {
+	unsigned int type;
+	unsigned int no;
+	int socket_fd;
+	int epoll_fd;
+	pthread_t thread_ar;
+	union {
+		struct {
+			unsigned int space_id;
+			struct {
+				struct { unsigned int send; unsigned int recv; } set;
+				struct { unsigned int send; unsigned int recv; } get;
+			} stat; 
+		} tarantool;
+		struct {
+			unsigned int database;
+		} redis;
+		struct { } memcached;
+		struct { } dynamodb;
+		struct { } aerospike;
+	};
+} nosql_t;
+
+nosql_t *nosql_connect(unsigned int type, const char *host, const char *port, unsigned int no, ...);
+void nosql_auto_read(nosql_t *nosql);
+void nosql_latency(nosql_t *nosql, unsigned int thread_max, unsigned int iteration_max, unsigned int batch_max);
+void nosql_insert(nosql_t *nosql, unsigned int iteration_no, const char *value, unsigned int value_size);
+void nosql_select(nosql_t *nosql, unsigned int iteration_no);
+
+// Tarantool.
+
+void nosql_tarantool_connect(nosql_t *nosql);
+void nosql_tarantool_insert(nosql_t *nosql, const char *key, unsigned int key_size, const char *value, unsigned int value_size);
+void nosql_tarantool_select(nosql_t *nosql, const char *key, unsigned int key_size);
+void *nosql_tarantool_auto_read_routine(void *arg); 
+void nosql_tarantool_auto_read(nosql_t *nosql);
+
+// Redis.
+
+void nosql_redis_connect(nosql_t *nosql);
+void nosql_redis_insert(nosql_t *nosql, const char *key, unsigned int key_size, const char *value, unsigned int value_size);
+void nosql_redis_select(nosql_t *nosql, const char *key, unsigned int key_size); 
+void *nosql_redis_auto_read_routine(void *arg); 
+void nosql_redis_auto_read(nosql_t *nosql); 
+
+// Memcached.
+
+typedef struct memcached_header_s {
+	uint8_t magic;
+	uint8_t opcode;
+	uint16_t key_length;
+	uint8_t extras_length;
+	uint8_t data_type;
+	union { uint16_t vbucket_id; uint16_t status; };
+	uint32_t total_body_length;
+	uint32_t opaque;
+	uint64_t cas;
+} __attribute__((packed)) memcached_header_t;
+typedef struct memcached_set_request_s {
+	memcached_header_t header;
+	struct { uint32_t flags; uint32_t expiration; } __attribute__((packed)) extras;
+} __attribute__((packed)) memcached_set_request_t;
+typedef struct memcached_get_request_s {
+	memcached_header_t header;
+} __attribute__((packed)) memcached_get_request_t;
+#define MEMCACHED_FILL_SET_REQUEST(r, opaque, key_size, value_size) \
+	r.header.magic = 0x80; \
+	r.header.opcode = 0x01; \
+	r.header.key_length = htons(key_size); \
+	r.header.extras_length = 0x08; \
+	r.header.vbucket_id = 0x0000; \
+	r.header.total_body_length = htonl(0x08 + key_size + value_size); \
+	r.header.opaque = htonl(opaque); \
+	r.header.cas = 0x0000000000000000; \
+	r.extras.flags = 0x00000000; \
+	r.extras.expiration = 0x00000000;
+#define MEMCACHED_FILL_GET_REQUEST(r, opaque, key_size) \
+	r.header.magic = 0x80; \
+	r.header.opcode = 0x00; \
+	r.header.key_length = htons(key_size); \
+	r.header.extras_length = 0x00; \
+	r.header.vbucket_id = 0x0000; \
+	r.header.total_body_length = htonl(key_size); \
+	r.header.opaque = htonl(opaque); \
+	r.header.cas = 0x0000000000000000;
+
+void nosql_memcached_connect(nosql_t *nosql);
+void *nosql_memcached_auto_read_routine(void *arg); 
+void nosql_memcached_auto_read(nosql_t *nosql);
+void nosql_memcached_insert(nosql_t *nosql, unsigned int opaque, const char *key, unsigned int key_size, const char *value, unsigned int value_size);
+void nosql_memcached_select(nosql_t *nosql, unsigned int opaque, const char *key, unsigned int key_size);
+
+// Stat.
+
+typedef struct stat_s {
+	struct { unsigned long long send; unsigned long long recv; } set;
+	struct { unsigned long long send; unsigned long long recv; } get;
+	struct { unsigned int count; double time; } latency;
+	unsigned int error;
+} stat_t;
+extern stat_t stat;
+
+// Pid.
+
+extern pid_t pid;
+
+#endif
---
+++ src/tarantool.c
@@ -0,0 +1,110 @@
+#include "nosql-benchmark.h"
+
+void nosql_tarantool_connect(nosql_t *nosql) {
+	char buffer[128];
+	unsigned int read_count = 0;
+	while(read_count < 128) {
+		int read_result = recv(nosql->socket_fd, buffer + read_count, 128 - read_count, MSG_NOSIGNAL);
+		if(read_result > 0) read_count += read_result;
+	}
+}
+
+void nosql_tarantool_insert(nosql_t *nosql, const char *key, unsigned int key_size, const char *value, unsigned int value_size) {
+	char buffer[1048576], *pbh, *pb;
+	pbh = buffer; pb = pbh + 5;
+
+	pb = mp_encode_map   (pb, 2); 
+	pb = mp_encode_uint  (pb, 0x00); // code
+	pb = mp_encode_uint  (pb, 0x02); // insert
+	pb = mp_encode_uint  (pb, 0x01); // sync
+	pb = mp_encode_uint  (pb, 0x01); // "yes"
+	pb = mp_encode_map   (pb, 2);
+	pb = mp_encode_uint  (pb, 0x10); // space-id
+	pb = mp_encode_uint  (pb, nosql->tarantool.space_id);	
+	pb = mp_encode_uint  (pb, 0x21); // tuple
+	pb = mp_encode_array (pb, 2);
+	pb = mp_encode_str   (pb, key, key_size);
+	pb = mp_encode_str   (pb, value, value_size);
+	mp_encode_uint32(pbh, pb - buffer - 5);
+	pbh = pb;
+
+	send(nosql->socket_fd, buffer, pb - buffer, MSG_NOSIGNAL);
+	__sync_add_and_fetch(&stat.set.send, 1);
+	__sync_fetch_and_add(&nosql->tarantool.stat.set.send, 1);				
+}
+
+void nosql_tarantool_select(nosql_t *nosql, const char *key, unsigned int key_size) {
+	char buffer[1048576], *pbh, *pb;
+	pbh = buffer; pb = pbh + 5;
+
+	pb = mp_encode_map   (pb, 2); 
+	pb = mp_encode_uint  (pb, 0x00); // code
+	pb = mp_encode_uint  (pb, 0x01); // select
+	pb = mp_encode_uint  (pb, 0x01); // sync
+	pb = mp_encode_uint  (pb, 0x01); // "yes"
+	pb = mp_encode_map   (pb, 6);
+	pb = mp_encode_uint  (pb, 0x10); // space-id
+	pb = mp_encode_uint  (pb, nosql->tarantool.space_id);	
+	pb = mp_encode_uint  (pb, 0x11);
+	pb = mp_encode_uint  (pb, 0);
+	pb = mp_encode_uint  (pb, 0x12);
+	pb = mp_encode_uint  (pb, 1);
+	pb = mp_encode_uint  (pb, 0x13);
+	pb = mp_encode_uint  (pb, 0);
+	pb = mp_encode_uint  (pb, 0x14);
+	pb = mp_encode_uint  (pb, 0);
+	pb = mp_encode_uint  (pb, 0x20);
+	pb = mp_encode_array (pb, 1);
+	pb = mp_encode_str   (pb, key, key_size);
+	mp_encode_uint32(pbh, pb - buffer - 5);
+	pbh = pb;
+
+	send(nosql->socket_fd, buffer, pb - buffer, MSG_NOSIGNAL);
+	__sync_add_and_fetch(&stat.get.send, 1);
+	__sync_fetch_and_add(&nosql->tarantool.stat.get.send, 1);				
+}
+
+void *nosql_tarantool_auto_read_routine(void *arg) {
+	nosql_t *nosql = (nosql_t*)arg;
+	struct epoll_event event;
+	while(1) {
+		if(epoll_wait(nosql->epoll_fd, &event, 1, -1)) {
+			char buffer[1048576];
+			unsigned int header_offset = 0;
+			while(header_offset < 5) {
+				int recv_result = recv(nosql->socket_fd, buffer + header_offset, 5 - header_offset, MSG_NOSIGNAL);
+				if(recv_result > 0) header_offset += recv_result;
+			}
+			
+			const char *cpb = buffer;
+			int length = mp_decode_uint(&cpb); // check if length > buffer
+			
+			header_offset = 0;
+			while(header_offset < length) {
+				int recv_result = recv(nosql->socket_fd, buffer + header_offset, length - header_offset, MSG_NOSIGNAL);
+				if(recv_result > 0) header_offset += recv_result;
+			}
+
+			cpb = buffer;
+
+			mp_decode_map(&cpb);
+			mp_decode_uint(&cpb);
+			
+			int status = mp_decode_uint(&cpb);
+
+			if(status) __sync_fetch_and_add(&stat.error, 1);
+
+			if(nosql->tarantool.stat.set.send > nosql->tarantool.stat.set.recv) {
+				__sync_fetch_and_add(&nosql->tarantool.stat.set.recv, 1);				
+				__sync_fetch_and_add(&stat.set.recv, 1);				
+			} else if(nosql->tarantool.stat.get.send > nosql->tarantool.stat.get.recv) {
+				__sync_fetch_and_add(&nosql->tarantool.stat.get.recv, 1);				
+				__sync_fetch_and_add(&stat.get.recv, 1);				
+			}
+		}
+	}
+}
+
+void nosql_tarantool_auto_read(nosql_t *nosql) {
+	pthread_create(&nosql->thread_ar, NULL, nosql_tarantool_auto_read_routine, nosql);
+}
